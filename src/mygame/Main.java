ba43075295b24373f7b103ae2cff4e2bbe47caf2
Eduardo <encarnacao.duds27@gmail.com>
2017-06-11 11:51:33 -0300

    Adicionado prato direito

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -21,11 +21,17 @@ import net.java.games.input.Component;
  */
 public class Main extends SimpleApplication {
 
-    private boolean initial = false;
-    private long timeInitial = 0l;
     private static final long TIME_MAX_SECONDS = 1000;
     private static final long TIME_MIN_SECONDS = 500;
-    private int direction = 1;
+    
+    
+    private boolean initialPratoEsquerdo  = false;
+    private long timeInitialPratoEsquerdo = 0l;
+    private int directionPratoEsquerdo    = 1;
+    
+    private boolean initialPratoDireito  = false;
+    private long timeInitialPratoDireito = 0l;
+    private int directionPratoDireito    = 1;
     
     public static void main(String[] args) {
         Main app = new Main();
@@ -41,26 +47,19 @@ public class Main extends SimpleApplication {
         
         //Models/drums_current8-scene_node/Cymbal.001/Circle.030-entity/Circle.030-ogremesh
         
-        Spatial pratoEsquerdo = assetManager.loadModel("Models/Circle.030.mesh.j3o");
-        
-        
-        Material boxMat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
-        boxMat.setColor("Ambient", ColorRGBA.Yellow); 
-        boxMat.setBoolean("UseMaterialColors", true); 
-        pratoEsquerdo.setMaterial(boxMat);
-        pratoEsquerdo.setName("Prato_Esquerdo");
-        pratoEsquerdo.rotate(0, 0, -0.4f);
+        criarPratoEsquerdo();
+        criarPratoDireito();
         
 
         //rootNode.attachChild(model);
-        rootNode.attachChild(pratoEsquerdo);
-        
+                
         /** A white ambient light source. */ 
         AmbientLight ambient = new AmbientLight();
         ambient.setColor(ColorRGBA.White);
         rootNode.addLight(ambient);         
         
-        timeInitial = System.currentTimeMillis();
+        timeInitialPratoEsquerdo = System.currentTimeMillis();
+        timeInitialPratoDireito = System.currentTimeMillis();
     }
     
     @Override
@@ -69,6 +68,7 @@ public class Main extends SimpleApplication {
         
                 
         movimentaPratoEsquerdo(tpf);
+        movimentaPratoDireito(tpf);
         
         /*System.out.println("X: " + rotation.getX() + 
                 " Y: " + rotation.getY() + 
@@ -84,31 +84,92 @@ public class Main extends SimpleApplication {
     }
     
     
+    
+    
+    private void criarPratoEsquerdo() {
+        Spatial pratoEsquerdo = assetManager.loadModel("Models/Circle.030.mesh.j3o");
+        
+        Material boxMat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
+        boxMat.setColor("Ambient", ColorRGBA.Yellow); 
+        boxMat.setBoolean("UseMaterialColors", true); 
+        pratoEsquerdo.setMaterial(boxMat);
+        pratoEsquerdo.setName("Prato_Esquerdo");
+        pratoEsquerdo.rotate(0, 0, -0.4f);
+        
+        rootNode.attachChild(pratoEsquerdo);
+
+    }
+    
+    private void criarPratoDireito() {
+        Spatial pratoDireito = assetManager.loadModel("Models/Circle.030.mesh.j3o");
+        
+        Material boxMat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
+        boxMat.setColor("Ambient", ColorRGBA.Yellow); 
+        boxMat.setBoolean("UseMaterialColors", true); 
+        pratoDireito.setMaterial(boxMat);
+        pratoDireito.setName("Prato_Direito");
+        pratoDireito.rotate(0, 0, -0.4f);
+        pratoDireito.setLocalTranslation(-5f, 0, 0);
+        
+        rootNode.attachChild(pratoDireito);
+
+    }
+    
+    
     private void movimentaPratoEsquerdo (float tpf) {
         Spatial pratoEsquerdo = rootNode.getChild("Prato_Esquerdo");
         Quaternion rotation   = pratoEsquerdo.getLocalRotation();
         
-        long time = System.currentTimeMillis() - timeInitial;
+        long time = System.currentTimeMillis() - timeInitialPratoEsquerdo;
         
         if (time < TIME_MIN_SECONDS) {
-            pratoEsquerdo.rotate(0, 0, tpf * direction);
+            pratoEsquerdo.rotate(0, 0, tpf * directionPratoEsquerdo);
         }
         
         if (time >= TIME_MIN_SECONDS) {
-            if (!initial) {
-                initial = true;
-                direction *= (-1);
+            if (!initialPratoEsquerdo) {
+                initialPratoEsquerdo    = true;
+                directionPratoEsquerdo *= (-1);
             }
-            pratoEsquerdo.rotate(0, 0, tpf * direction);
+            pratoEsquerdo.rotate(0, 0, tpf * directionPratoEsquerdo);
         }
         
         if (time > TIME_MAX_SECONDS) {
-            initial = false;
-            timeInitial = System.currentTimeMillis();
-            direction *= (-1);
+            initialPratoEsquerdo     = false;
+            timeInitialPratoEsquerdo = System.currentTimeMillis();
+            directionPratoEsquerdo  *= (-1);
         }
         
-        pratoEsquerdo.rotate(0, 0, tpf * direction);        
+        pratoEsquerdo.rotate(0, 0, tpf * directionPratoEsquerdo);
+    }
+    
+    
+    
+    private void movimentaPratoDireito (float tpf) {
+        Spatial pratoDireito = rootNode.getChild("Prato_Direito");
+        Quaternion rotation  = pratoDireito.getLocalRotation();
+        
+        long time = System.currentTimeMillis() - timeInitialPratoDireito;
+        
+        if (time < TIME_MIN_SECONDS) {
+            pratoDireito.rotate(0, 0, tpf * directionPratoDireito);
+        }
+        
+        if (time >= TIME_MIN_SECONDS) {
+            if (!initialPratoDireito) {
+                initialPratoDireito    = true;
+                directionPratoDireito *= (-1);
+            }
+            pratoDireito.rotate(0, 0, tpf * directionPratoDireito);
+        }
+        
+        if (time > TIME_MAX_SECONDS) {
+            initialPratoDireito     = false;
+            timeInitialPratoDireito = System.currentTimeMillis();
+            directionPratoDireito  *= (-1);
+        }
+        
+        pratoDireito.rotate(0, 0, tpf * directionPratoDireito);
     }
     
 }
